aea70d54456e7d03d24222a40fef86caebe283b7
Azizbek2396 <aziznurmuhammadov1996@gmail.com>
2020-11-25 11:48:45 +0500

    Eighth day

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class BasketController extends Controller
@@ -20,8 +21,35 @@ class BasketController extends Controller
         return view('basket', compact('order'));
     }
 
+    public function basketConfirm(Request $request)
+    {
+        $orderId = session('orderId');
+
+        if (is_null($orderId)) {
+            return redirect()->route('index');
+        }
+        $order = Order::find($orderId);
+        $success = $order->saveOrder($request->name, $request->phone);
+
+        if ($success) {
+            session()->flash('success', 'Ваш заказ принят в обработку!');
+        } else {
+            session()->flash('warning', 'Случилось ошибка!');
+        }
+//        dd($request->all());
+        return redirect()->route('index');
+    }
+
     public function basketPlace() {
-        return view('order');
+        $orderId = session('orderId');
+
+        if (is_null($orderId)) {
+            return redirect()->route('index');
+        }
+
+        $order = Order::find($orderId);
+//        dd($order);
+        return view('order', compact('order'));
     }
 
     public function basketAdd($productId)
@@ -46,6 +74,10 @@ class BasketController extends Controller
             $order->products()->attach($productId);
         }
 
+        $product = Product::find($productId);
+
+        session()->flash('success', 'Добавлен товар ' . $product->name);
+
         return redirect()->route('basket');
 
 //        return view('basket', compact('order'));
@@ -68,6 +100,9 @@ class BasketController extends Controller
             $pivotRow->update();
 //            dd($pivotRow);
         }
+
+        $product = Product::find($productId);
+        session()->flash('warning', 'Удалён товар ' . $product->name);
 
         return redirect()->route('basket');
     }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/basket' , [\App\Http\Controllers\BasketController::class, 'basket']
 Route::get('/basket/place' , [\App\Http\Controllers\BasketController::class, 'basketPlace'])->name('basket-place');
 Route::post('/basket/add/{id}', [\App\Http\Controllers\BasketController::class, 'basketAdd'])->name('basket-add');
 Route::post('/basket/remove/{id}', [\App\Http\Controllers\BasketController::class, 'basketRemove'])->name('basket-remove');
+Route::post('/basket/place' , [\App\Http\Controllers\BasketController::class, 'basketConfirm'])->name('basket-confirm');
 
 Route::get('/{category}' , [\App\Http\Controllers\MainController::class, 'category'])->name('category');
 Route::get('/{category}/{product?}' , [\App\Http\Controllers\MainController::class, 'product'])->name('product');
